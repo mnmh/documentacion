4469a7bbeba8b3e0e3bc8d4ac4c3c7a2b4d7c7a0
Laura Junco <lc.junco10@uniandes.edu.co>
2020-05-08 12:50:19 -0500

    Pequeñas correcciones al texto

--- src/paginas/SistemaDiseno.js
+++ src/paginas/SistemaDiseno.js
@@ -18,15 +18,15 @@ class SistemaDiseno extends Component {
 
                 <div id="porque">
                     <h2>¿Porque es necesario un sistema de diseño?</h2>
-                    <p> <b> Control de la representación:</b> Cuando se diseña a corto plazo y para problemas específicos empieza a aparecer una sobreabundancia de estilos y convenciones que se convierten  en imposibles de mantener para futuros productos. Un sistema de diseño, provee soluciones que son transversales a los diferentes productos digitales de manera que lo que se desarrolla una vez puede ser utilizado en múltiples ocasiones.</p>  
+                    <p> <b> Control de la representación: </b> Cuando se diseña a corto plazo y para problemas específicos empieza a aparecer una sobreabundancia de estilos y convenciones que se convierten  en imposibles de mantener para futuros productos. Un sistema de diseño, provee soluciones que son transversales a los diferentes productos digitales de manera que lo que se desarrolla una vez puede ser utilizado en múltiples ocasiones.</p>  
 
-                    <p> <b> Control de la representación:</b> Cuando se diseña a corto plazo y para problemas específicos empieza a aparecer una sobreabundancia de estilos y convenciones que se convierten  en imposibles de mantener para futuros productos. Un sistema de diseño, provee soluciones que son transversales a los diferentes productos digitales de manera que lo que se desarrolla una vez puede ser utilizado en múltiples ocasiones.</p> 
+                    <p> <b> Sistema central actualizado: </b> un sistema de diseño crea un lugar centralizado y constantemente actualizado donde los miembros del equipo pueden consultar y seguir las guías y estándares establecidos. Esto es especialmente importante para equipos distribuidos o que cambien constantemente.</p> 
 
-                    <p> <b>Sistema central actualizado: </b>componentes estandarizados y utilizados constantemente crean una interfaz fácil de leer y navegar para el usuario. Además permite a los diseñadores preocuparse menor por el diseño de estilos y enfocarse más en la experiencia de usuario.</p> 
+                    <p> <b> Interfaz predecible y fácil de entender: </b>componentes estandarizados y utilizados constantemente crean una interfaz fácil de leer y navegar para el usuario. Además permite a los diseñadores preocuparse menor por el diseño de estilos y enfocarse más en la experiencia de usuario.</p> 
 
                     <p> <b> Prototipado e iteración rápida:</b> un sistema de diseño provee una librería de bloques y recursos visuales a disposición del equipo para experimentar y crear prototipos mucho más rápido. De la misma manera es posible hacer iteraciones de estilo en los productos existentes de forma más eficiente ya que la actualización de algún componente se refleja inmediatamente en todos los productos digitales.</p> 
 
-                    <p> <b> Accesibilidad y usabilidad:</b> Al diseñar desde el nivel de componentes se reduce la cantidad de código  (css y javascript)  necesario en un contenido digital, en consecuencia los productos son optimizados para personas con conexiones a internet lentas o computadores viejos.</p> 
+                    <p> <b> Accesibilidad y usabilidad:</b> al diseñar desde el nivel de componentes se reduce la cantidad de código  (css y javascript)  necesario en un contenido digital, en consecuencia los productos son optimizados para personas con conexiones a internet lentas o computadores viejos.</p> 
                 </div>
             </section>
             </>
