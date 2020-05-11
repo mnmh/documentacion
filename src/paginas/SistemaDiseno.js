0032955a8ce3b12a47786af5a9d4e2d3bd9193fd
Laura Junco <lc.junco10@uniandes.edu.co>
2020-05-11 11:37:42 -0500

    Agregar textos de lenguaje visual

--- src/paginas/SistemaDiseno.js
+++ src/paginas/SistemaDiseno.js
@@ -12,7 +12,7 @@ class SistemaDiseno extends Component {
             <>
             <section id="insideDoc">
                 <div id="que-es">
-                    <h2>Sistema de diseño de contenidos digitales del museo de memoria de Colombia</h2>
+                    <h2>Sistema de diseño de contenidos digitales del Museo de Memoria de Colombia</h2>
                     <p>El sistema de diseño es una colección de  patrones, componentes y herramientas reutilizables, guiados por estándares en DDHH en entornos digitales, que se entrelazan y iteran para crear contenidos digitales coherentes, consistentes y unificados con los objetivos del programa digital. Principalmente permite a los equipos crear mejores productos de manera más eficiente así como escalar el diseño para los distintos contenidos digitales. </p>
                 </div>
 
@@ -27,6 +27,45 @@ class SistemaDiseno extends Component {
                     <p> <b> Prototipado e iteración rápida:</b> un sistema de diseño provee una librería de bloques y recursos visuales a disposición del equipo para experimentar y crear prototipos mucho más rápido. De la misma manera es posible hacer iteraciones de estilo en los productos existentes de forma más eficiente ya que la actualización de algún componente se refleja inmediatamente en todos los productos digitales.</p> 
 
                     <p> <b> Accesibilidad y usabilidad:</b> al diseñar desde el nivel de componentes se reduce la cantidad de código  (css y javascript)  necesario en un contenido digital, en consecuencia los productos son optimizados para personas con conexiones a internet lentas o computadores viejos.</p> 
+                </div>
+                <div id="lenguaje-visual">
+                    <h2>Lenguaje visual</h2>
+                    <p>El lenguaje visual define el propósito, estilo y forma de uso de los elementos gráficos en los productos virtuales.</p>
+
+                    {/* Color */}
+                    <h2>Color</h2>
+                    <p><b>Colores principales:</b> <br/>
+                    Azul: #225DDC
+                    <br/>
+                    Amarillo: #FEB639
+                    </p>
+
+                    <p><b>Tonos grises:</b> <br/>
+                    Gris oscuro: #3B3B3B
+                    <br/>
+                    Gris claro: #9A9A9A
+                    </p>
+
+                    <p><b>Otros colores:</b> <br/>
+                    Turquesa: #00AE9D
+                    <br/>
+                    Verde: #00A650
+                    <br/>
+                    Naranja: #F57F29
+                    </p>
+
+                    {/* Tipografía */}
+                    <h2>Tipografía</h2>
+                    <p><b>Para títulos y encabezados:</b> <br/>
+                    <a></a>Fuente Inter @googlefonts diseñada por Rasmus Anderson </p>
+
+                    <p><b>Para cuerpo de texto::</b> <br/>
+                    Fuente Crimson @googlefonts diseñada por Jacques Le Bailly </p>
+
+                </div>
+
+                <div id="bloques">
+                    
                 </div>
             </section>
             </>
