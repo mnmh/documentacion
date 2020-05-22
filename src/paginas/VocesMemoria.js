d18da3a38b38c887d22fafe6737831196cfa8228
Laura Junco <lc.junco10@uniandes.edu.co>
2020-05-22 11:16:26 -0500

    Empezar a agregar los textos de personalización visual

--- src/paginas/VocesMemoria.js
+++ src/paginas/VocesMemoria.js
@@ -59,10 +59,57 @@ class VocesMemoria extends Component {
                     </ul>
                 </div>
                 <div id="bga-desarrollo">
-                    <h2>Voces de la memoria</h2>
+                    <h2>Scripts para el desarrollo</h2>
+                    <ul>
+                        <li> <b>1. Instalar node.js </b></li>
+                        <li><b>2. Clonar repositorio: </b> git clone https://github.com/mnmh/curaduria-bga.git</li>
+                        <li><b>3. Instalar las dependencias: </b> npm install</li>
+                        <li><b>4. Compilar css una vez:</b>  npm run css </li>
+                        <li><b>5. Compilar css cada vez que se realicen cambios:</b> npm run watch</li>
+                        <li><b>6. Arrancar un servidor local: </b> node index.js</li>
+                    </ul>
                 </div>
                 <div id="bga-visual">
-                    <h2>Voces de la memoria</h2>
+                    <h2>Personalización del diseño visual</h2>
+                    <p>Estas son las instrucciones para cambiar la apariencia visual del especial</p>
+
+                    {/*Tipografía*/}
+                    <h2>Tipografía</h2>
+                    <p>
+                        Para modificar las tipografías utilizadas en el especial primero se deben agregar los archivos a la carpeta <b>fonts/</b>. En segundo lugar se deben agregar los enlaces a las fuentes en el archivo <b>scss/estilos.scss</b>. Finalmente en el archivo scss reemplazar las variables <b>$fuente-titulos</b> y <b>$fuente-textos</b> por las tipografías a utilizar
+                    </p>
+
+                    {/*Color*/}
+                    <h2>Color</h2>
+                    <p>
+                        Para modificar los colores utilizadas en el especial basta con reemplazar las variables <b> $primario</b>, <b>$secundario</b> y <b>$fondo</b> en el archivo <b>scss/estilos.scss</b>.
+                    </p>
+
+                    {/*Audio*/}
+                    <h2>Reproductor de Audio</h2>
+                    <p>
+                    Para reemplazar los botones del reproductor de audio hay que reemplazar los archivos <b>next.svg</b>, <b>pause.svg</b>, <b>play.svg</b>, <b>prev.svg</b> de la carpeta <b>svg/</b>
+                    </p>
+
+                    {/*Ilustraciones*/}
+                    <h2>Ilustraciones</h2>
+                    <p>
+                    Para el funcionamiento de las funcionalidades del especial, cada ilustración debe ser <b> un único path en formato svg</b> cuyo código se pega directamente en el html del especial. Para construir las ilustraciones se puede usar un software como illustrator.ai, figma, adobe.xd, etc. 
+                    </p>
+
+
+
+                    {/*<div className="listado">
+                    <div className="item btm">
+                            <div className="num">1</div>
+                            <div className="contenido">
+                            <p>Tipografía</p>
+
+                            </div>
+                        </div>
+
+                    </div> */}
+
                 </div>
                 <div id="bga-scripts">
                     <h2>Voces de la memoria</h2>
