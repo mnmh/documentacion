868004beb1119ca7d7cf9161f2ba0505f5e11e24
Laura Junco <lc.junco10@uniandes.edu.co>
2020-05-22 10:46:53 -0500

    Crear componente VocesMemoria

---
+++ src/paginas/VocesMemoria.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+import Nav from '../components/Nav'
+
+class VocesMemoria extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <>
+            <section id="insideDoc">
+                <div>
+                    <h2>Voces de la memoria</h2>
+                </div>
+            </section>
+            </>
+        )
+    }
+}
+
+export default VocesMemoria;
